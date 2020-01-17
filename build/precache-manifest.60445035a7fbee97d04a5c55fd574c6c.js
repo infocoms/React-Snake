73040a94b600ec4bd02821115a8011d4b7c12116
kevin comyn <kevincomyn@gmail.com>
2020-01-17 10:58:32 +0100

    Add files via upload

---
+++ build/precache-manifest.60445035a7fbee97d04a5c55fd574c6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e812085fe0d85afd3f4ed655d50db156",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d70ecfb3d77a95919dd",
+    "url": "/static/css/main.cc177e44.chunk.css"
+  },
+  {
+    "revision": "39a2bc5c3f162081e985",
+    "url": "/static/js/2.e0c0c5c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e0c0c5c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d70ecfb3d77a95919dd",
+    "url": "/static/js/main.e6015259.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
