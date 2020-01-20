8792d21e3d6b837cd340e1ce0a6cd954d837bfbb
kevin comyn <kevincomyn@gmail.com>
2020-01-20 09:26:13 +0100

    Delete Boost.js

--- src/Boost.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default props => {
-    const style = {
-        left: `${props.block[0]}%`,
-        top: `${props.block[1]}%`
-    };
-    return <div className="boost block" style={style}/>;
-};
